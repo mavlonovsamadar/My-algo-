7f3a0a7b6a3908da185d25ccdc991fcc41838f19
samandar92 <mavlonovsamandar1992@gmail.com>
2022-12-02 19:51:20 +0500

    fix

---
+++ 1. 2.12.2022 MyName/MyName.js
@@ -0,0 +1,23 @@
+// function MyName(name) {
+//     return `Mening ismim ${name}`
+// }
+// console.log(MyName("Samandar"));
+
+/////////////////////////////////////////////////////////////
+
+// function MyName(name, age, hobby) {
+//     return `Mening ismim ${name}. Mening yoshim ${age}da va yoqtirgan hobbim ${hobby}.`
+// }
+// console.log(MyName("Samandar", 30, "futbol"));
+
+/////////////////////////////////////////////////////////////
+
+// function getInfo() {
+//     let data = {
+//         a: "Samandar",
+//         b: 30,
+//         c: "nexia-3"
+//     }
+//     return `Mening ismim ${data.a}. yoshim ${data.b}da va Moshinam ${data.c}.`
+// }
+// alert(getInfo());
