781f1d2e5d484d1e19c623ec8fdef1ecff01b978
samandar92 <mavlonovsamandar1992@gmail.com>
2022-12-02 20:13:05 +0500

    fix

--- 1. 2.12.2022 MyName/MyName.js
+++ 1. 2.12.2022 MyName/MyName.js
@@ -12,13 +12,42 @@
 
 /////////////////////////////////////////////////////////////
 
-function getInfo(a, b, c) {
-    let data = {
-        a: a,
-        b: b,
-        c: c
-    }
+// function getInfo(a, b, c) {
+//     let data = {
+//         a: a,
+//         b: b,
+//         c: c
+//     }
    
-    return `Mening ismim ${data.a}. yoshim ${data.b}da va Moshinam ${data.c}.`
+//     return `Mening ismim ${data.a}. yoshim ${data.b}da va Moshinam ${data.c}.`
+// }
+// alert(getInfo("Samandar", 30, "nexia-3"));
+
+/////////////////////////////////////////////////////////////
+
+// function addition(num) {
+//     return num+1
+// }
+// console.log(addition(9));
+
+/////////////////////////////////////////////////////////////
+
+// function Secund(min) {
+//     return min*60
+// }
+// console.log(Secund(3));
+
+/////////////////////////////////////////////////////////////
+
+// function triArea(a,b) {
+//     return a*b/2
+// }
+// console.log(triArea(10,10));
+
+/////////////////////////////////////////////////////////////
+
+
+function Sum(a,b) {
+    return a+b
 }
-alert(getInfo("Samandar", 30, "nexia-3"));
+console.log(Sum(-3,-9));
